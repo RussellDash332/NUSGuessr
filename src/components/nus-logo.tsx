d1ef4b878b7fabb3903633129889e2d8a8013bfe
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2025-12-18 09:20:59 +0000

    Fix favicon path bug

--- src/components/nus-logo.tsx
+++ src/components/nus-logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function NUSLogo({ className }: { className?: string }) {
   return (
     <Image
-      src="/favicon.ico"
+      src="./favicon.ico"
       alt="NUSGuessr Logo"
       width={40}
       height={40}
